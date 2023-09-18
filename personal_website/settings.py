1a698e3399d29bcbeffe358c2bbb11b800947d0f
Yash Varshney <107148830+varshney-yash@users.noreply.github.com>
2023-09-18 22:26:05 +0530

    Update settings.py

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -156,3 +156,7 @@ CSRF_TRUSTED_ORIGINS = ['https://rl-app-production.up.railway.app']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+

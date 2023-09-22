85c543519acb832c535f1bcc031cf593375aa657
Yash Varshney <107148830+varshney-yash@users.noreply.github.com>
2023-09-22 16:11:28 +0530

    Set debug false for deployment

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j%8by-#t2h_=(eqcm1qys)sh74&3@mkz&y&mcwyyw$e*e0q=*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
